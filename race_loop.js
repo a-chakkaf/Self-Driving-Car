b4d80ca2b85895142c69dc5c3c0a4b44f80c8db3
Abdelaziz Chakkaf <achakkaf@c1r8p7.1337.ma>
2026-02-23 16:40:35 +0000

    race track is fished

--- race_loop.js
+++ race_loop.js
@@ -12,13 +12,14 @@
 
 
 MARGIN = 50
-N = 7
+N = 27
 
 class track_generator {
     constructor(width, height) {
         this.width = width - MARGIN;
         this.height = height - MARGIN;
         this.randomPoints = [];
+        this.trackCorners = [];
     }
 
     #generateXY() {
@@ -27,7 +28,7 @@ class track_generator {
         return { x: val_x, y: val_y };
     }
 
-    #generateRandomNumbers() {
+    generateRandomNumbers() {
         for (let i = 0; i < N; i++) {
             this.randomPoints.push(this.#generateXY());
         }
@@ -48,9 +49,10 @@ class track_generator {
         ctx.stroke();
     }
 
-    drawPoints(ctx) {
-        for (let i = 0; i < N; i++) {
-            this.drawPoint(ctx, this.randomPoints[i].x, this.randomPoints[i].y)
+    // POINTS is an array
+    drawPoints(ctx, points, radius = 3, color = "#FFFF00") {
+        for (let i = 0; i < points.length; i++) {
+            this.drawPoint(ctx, points[i], radius, color)
         }
     }
 
@@ -63,7 +65,7 @@ class track_generator {
         let current = anchorPoint;
         let nextPoint = this.randomPoints[0];
 
-        for (let i = 0; i < N; i++) {
+        for (let i = 0; i < this.randomPoints.length; i++) {
             let checking = this.randomPoints[i];
             if (checking === current) {
                 continue;
@@ -79,25 +81,71 @@ class track_generator {
         ctx.strokeStyle = color;
         ctx.lineWidth = lineWidth;
         ctx.beginPath();
+        ctx.lineJoin = "round"
         ctx.moveTo(p1.x, p1.y);
         ctx.lineTo(p2.x, p2.y);
         ctx.stroke();
     }
 
-    drawLines(ctx) {
-        let anchorPoint = this.#findLeftPoint();
-        for (let i = 0; i < N; i++) {
+    cornerPoints() {
+        // Save the very first point so we know when to stop
+        let firstAnchor = this.#findLeftPoint();
+        let anchorPoint = firstAnchor;
+        do {
+            this.trackCorners.push(anchorPoint);
             let bp = this.bestPoint(anchorPoint);
-            this.drawPoint(ctx, bp, 2, "#FF5FF0");
-            this.#drawline(ctx, anchorPoint, bp);
             anchorPoint = bp;
+        } while (anchorPoint !== firstAnchor); // Stop the moment we connect back to the start!
+    }
+
+    linkCorners(ctx) {
+        ctx.beginPath();
+        ctx.moveTo(this.trackCorners[0].x, this.trackCorners[0].y);
+
+        for(let i = 1; i < this.trackCorners.length; i++){
+            ctx.lineTo(this.trackCorners[i].x, this.trackCorners[i].y);
         }
+
+        ctx.closePath();
+
+        ctx.lineWidth = 100;
+        ctx.strokeStyle = "gray";
+        ctx.lineJoin = "round";
+
+        ctx.stroke();
+    }
+
+    cornerCuttingMath(a, b) {
+        let x = a.x * 0.75 + b.x * 0.25;
+        let y = a.y * 0.75 + b.y * 0.25;
+        let X1 = { x, y };
+        x = a.x * 0.25 + b.x * 0.75;
+        y = a.y * 0.25 + b.y * 0.75;
+        let X2 = { x, y };
+
+        return [X1, X2];
+    }
+
+    cornerCutting() {
+        let points = [];
+
+        // points.push(this.trackCorners[0]);
+        for (let i = 0; i < this.trackCorners.length; i++) {
+            let A = this.trackCorners[i];
+            let B = this.trackCorners[(i + 1) % this.trackCorners.length];
+            let [X1, X2] = this.cornerCuttingMath(A, B);
+
+            points.push(X1, X2);
+        }
+        this.trackCorners = points;
     }
 
     track_init(ctx) {
-        this.#generateRandomNumbers();
-        this.drawPoints(ctx);
-        this.drawLines(ctx);
+        this.generateRandomNumbers();
+        this.cornerPoints();
+        this.cornerCutting();
+        this.linkCorners(ctx);
+        // this.drawPoints(ctx, this.trackCorners);
     }
 }
 
